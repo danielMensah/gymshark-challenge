6460f09f29acafc83810f27f56b9a66655c78adc
Daniel <mensahezros@gmail.com>
2020-10-22 00:25:35 +0100

    added to aws lambda

--- main.go
+++ main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"math"
+	"net/http"
 )
 
-func main() {
-	fmt.Print("Hello World")
+type Data struct {
+	Quantity int `json:"quantity"`
+	Packs []int `json:"packs"`
 }
 
+type Response struct {
+	Packs []int
+}
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	data := &Data{}
+
+	_ = json.Unmarshal([]byte(request.Body), data)
+
+	// default quantity is 0
+	if data.Quantity < 1 {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body: "Quantity must be set or more than 0",
+		}, nil
+	} else if data.Packs == nil || len(data.Packs) == 0 {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body: "No packs available!",
+		}, nil
+	}
+
+	var res []int
+	result := order(data.Quantity, data.Packs, res)
+
+	response := Response{Packs: result}
+	var jsonData []byte
+
+	jsonData, _ = json.Marshal(response)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body: string(jsonData),
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
 
 func order (quantity int, packs []int, res []int) []int {
 	if contains(quantity, packs) {
